7e0278a0f17a2da07d7ce9ebe65d7793461a8a08
chenxinda <chenxinda520@qq.com>
2018-07-30 15:16:30 +0800

    123

---
+++ src/node-server.js
@@ -0,0 +1,34 @@
+var fs = require('fs')
+var http = require('http')
+var url = require('url')
+var qiniu = require('qiniu')
+
+var server = http.createServer(function(request, response){
+        let parseUrl = url.parse(request.url,true)
+        let path = parseUrl.pathname
+        if(path === '/uptoken'){
+            response.setHeader('Content-Type','text/html; charset=utf-8')
+            response.setHeader('Access-Content-Allow-Origin','*')
+            let json = fs.readFileSync('./qiniu-key.json')
+            json = JSON.parse(json)
+            let {accessKey, secretKey} = json
+            
+            let mac = new qiniu.auth.digest.Mac(accessKey, secretKey);
+            let options = {
+                scope: 'music163',
+                expires: 7200
+            };
+            var putPolicy = new qiniu.rs.PutPolicy(options);
+            var uploadToken= putPolicy.uploadToken(mac);
+            response.write(`
+                "uptoken": "${uploadToken}"
+            `)
+            response.end()
+        }else{
+            response.setHeader('Content-Type','text/html; charset=utf-8')
+            response.write('地址错误')
+            response.end()
+        }      
+    })
+server.listen(8888)
+console.log('打开localhost:8888')
